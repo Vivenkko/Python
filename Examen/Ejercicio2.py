3716f59d267627b1f9092e1bcc6d200a29145990
Vivenkko <valhallahacker@gmail.com>
2017-11-07 13:21:28 +0100

    examen cambiado

--- Examen/Ejercicio2.py
+++ Examen/Ejercicio2.py
@@ -1,11 +1,2 @@
-opcion = 0
-resultado = 0
-
-while opcion.isdigit():
-    print "Introduzca un numero:"
-    opcion = raw_input()
-    if opcion.isdigit():
-        int(opcion)
-        resultado += opcion
-
-print "La suma total es "+resultado
+lista = [1,2,3,4]
+print (lambda x: sum(x))(lista)
